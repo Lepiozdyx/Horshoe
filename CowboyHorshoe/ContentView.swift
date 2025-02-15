4046434f57e23019e07887333017268beedf1697
Developer <Lepiozdyx@gmail.com>
2025-02-15 10:47:11 -0300

    Initial Commit

---
+++ CowboyHorshoe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CowboyHorshoe
+//
+//  Created by Alex on 15.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
