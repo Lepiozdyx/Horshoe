d99625c6ef91b91fff98fc26dd8033c8ea9995ae
Developer <Lepiozdyx@gmail.com>
2025-02-18 10:49:28 -0300

    add views

---
+++ CowboyHorshoe/Screens/Components/EndgameView.swift
@@ -0,0 +1,81 @@
+//
+//  EndgameView.swift
+//  CowboyHorshoe
+//
+//  Created by Alex on 18.02.2025.
+//
+
+import SwiftUI
+
+struct EndgameView: View {
+    
+    var isVictory: Bool
+    
+    let goToMenuAction: () -> ()
+    let tryAgainAction: () -> ()
+    
+    var body: some View {
+        ZStack {
+            BackgroundView(imageName: isVictory ? .bg1 : .bg2)
+            
+            ZStack {
+                Image(.underlay5)
+                    .resizable()
+                    .frame(maxWidth: 400, maxHeight: 400)
+                    .overlay(alignment: .topTrailing) {
+                        XmarkButtonView()
+                    }
+                    .overlay(alignment: .top) {
+                        Image(isVictory ? .win : .lose)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 180)
+                            .offset(x: 20, y: 45)
+                    }
+                    .padding()
+                
+                VStack {
+                    if isVictory {
+                        HStack {
+                            Image(.horseshoe)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 35)
+                            
+                            Image(.plus10)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 60)
+                        }
+                        .offset(x: 20)
+                    }
+                    
+                    HStack(spacing: 20) {
+                        Button {
+                            goToMenuAction()
+                        } label: {
+                            Image(.home)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80)
+                        }
+                        
+                        Button {
+                            tryAgainAction()
+                        } label: {
+                            Image(isVictory ? .next : .tryAgain)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: isVictory ? 110 : 80)
+                        }
+                    }
+                    .offset(x: 20)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    EndgameView(isVictory: true, goToMenuAction: {}, tryAgainAction: {})
+}
